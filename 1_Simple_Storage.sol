0920abe7b6693d0bc04489dc1c576cb42430251d
johnrobertmcc <john.robert.mcc@gmail.com>
2022-02-16 22:50:53 -0500

    first examples

---
+++ 1_Simple_Storage.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.4.16 < 0.9.0;
+
+contract SimpleStorage {
+    uint public storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+
+    function deleteAll() public returns (uint){
+        storedData = 0;
+        return storedData;
+    }
+
+
+}
