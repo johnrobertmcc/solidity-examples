88c29bedfbc8562e8bb5020b362c9d6a00051a33
johnrobertmcc <john.robert.mcc@gmail.com>
2022-02-17 22:22:29 -0500

    added new functionality; added deal;

--- 3_Data_Types.sol
+++ 3_Data_Types.sol
@@ -3,7 +3,13 @@ pragma solidity >=0.7.0 <0.9.0;
 contract myGame {
 
     uint public playerCount = 0;
+    uint public pot = 0;
+    address public dealer;
+    uint public payableAmount;
+    
     mapping(address => Player) public players;
+    
+    Player[] public playersInGame;
 
     enum Level {Novice, Intermediate, Advanced}
 
@@ -12,14 +18,52 @@ contract myGame {
         Level playerLevel;
         string firstName;
         string lastName;
+        uint createdTime;
     }
 
-    function addPlayer(string memory firstName, string memory lastName) public {
-        players[msg.sender] = Player(msg.sender, Level.Novice, firstName, lastName);
-        playerCount += 1;
+    constructor() {
+        dealer = msg.sender;
+    }
+    
+    function addPlayer(string memory firstName, string memory lastName) private {
+        Player memory newPlayer = Player(msg.sender, Level.Novice, firstName, lastName, block.timestamp);
+        players[msg.sender] = newPlayer;
+        playersInGame.push(newPlayer);
     }
 
-    function getPlayerLevel(address playerAddress) public view returns(Level) {
-        return players[playerAddress].playerLevel;
+    function getPlayerLevel(address playerAddress) private view returns(Level) {
+        Player storage player = players[playerAddress];
+        return player.playerLevel;
+    }
+
+    function changePlayerLevel(address playerAddress) private {
+        Player storage player = players[playerAddress];
+        if(block.timestamp >= player.createdTime + 20) {
+            player.playerLevel = Level.Intermediate;
+        }
+    }
+
+    function joinGame(string memory firstName, string memory lastName) payable public {
+        require(msg.value == 25 ether, "The joining fee is 25 ether.");
+        if(payable(dealer).send(msg.value)) {
+            addPlayer(firstName, lastName);
+            playerCount += 1;
+            pot += 25;
+        }
+    }
+
+    function payOutWinners(address loserAddress) payable public {
+        require(msg.sender == dealer, "Only the dealer can pay out the winners.");
+        require(msg.value == pot * (1 ether), "This is where it's going wrong");
+        uint payoutPerWinner = msg.value / (playerCount - 1);
+        payableAmount = payoutPerWinner;
+
+        for(uint i=0; i < playersInGame.length; i++) {
+            address currentPlayerAddress = playersInGame[i].playerAddress;
+            if(currentPlayerAddress != loserAddress) {
+               payable(currentPlayerAddress).transfer(payoutPerWinner);
+            }
+
+        }
     }
 }
--- 4_Modifiers.sol
+++ 4_Modifiers.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.7.0 < 0.9.0;
 
 contract SimpleCoin {
+    uint contractStartTime;
     address public minter;
     mapping ( address => uint) public balances;
 
@@ -21,15 +22,21 @@ contract SimpleCoin {
         _;
     }
 
+    modifier oneWeekPassed(uint startTime) {
+        require(block.timestamp >= startTime + 604800);
+        _;
+    }
+
     constructor() {
         minter = msg.sender;
+        contractStartTime = block.timestamp;
     }
 
     function mint(address receiver, uint amount) public onlyMinter amountGreaterThan(amount) {
         balances[receiver] += amount;
     }
 
-    function send(address receiver, uint amount) public balanceGreaterThanAmount(amount) {
+    function send(address receiver, uint amount) public balanceGreaterThanAmount(amount) oneWeekPassed(contractStartTime) {
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
         emit Sent(msg.sender, receiver, amount);
